2b7a3eed97e0a131384578694a7ec1b98fdd3060
hossamdev <hossamdev14@gmail.com>
2024-03-12 11:46:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import { FaRegSmile } from "react-icons/fa";
+import { ImSad } from "react-icons/im";
+import './App.css'
+
+function App() {
+  
+  return (
+    <main>
+      <h2>Dialy Mood Tracker</h2>
+    </main>
+  )
+}
+
+export default App
