6ab20db445da4a0cc7f7d0b35cd6a878797c55af
hossamdev <hossamdev14@gmail.com>
2024-03-12 11:54:11 +0000

    create simple app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import react from 'react';
 import { FaRegSmile } from "react-icons/fa";
 import { ImSad } from "react-icons/im";
 import './App.css'
@@ -7,7 +6,13 @@ function App() {
   
   return (
     <main>
-      <h2>Dialy Mood Tracker</h2>
+      <article>
+        <h1 className='text-center'>How are you feeling today?</h1>
+        <h1 className='flex justify-between my-7 text-7xl text-blue-400'>
+          <FaRegSmile />
+          <ImSad />
+        </h1>
+      </article>
     </main>
   )
 }
